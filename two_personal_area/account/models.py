4ad60aefde1d124f07d0cc20268c1f27ddc65a5f
Lupuna <terragod427@gmail.com>
2024-05-19 17:43:39 +0700

    complete db structure

---
+++ two_personal_area/account/models.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MaxValueValidator
+from django.utils.translation import gettext_lazy as _
+from django.db import models
+
+
+class Client(AbstractUser):
+    phone = models.PositiveIntegerField(_('phone number'), blank=True, null=True)
+
+
+class Employee(models.Model):
+    client = models.OneToOneField(Client, on_delete=models.PROTECT, related_name='employee')
+    employee_rating = models.PositiveIntegerField(default=100, validators=[
+        MaxValueValidator(100),
+    ])
+
+    def __str__(self):
+        return self.client.username
+
+
+class Customer(models.Model):
+    client = models.OneToOneField(Client, on_delete=models.PROTECT, related_name='customer')
+    customer_rating = models.PositiveIntegerField(default=100, validators=[
+        MaxValueValidator(100),
+    ])
+
+    def __str__(self):
+        return self.client.username
