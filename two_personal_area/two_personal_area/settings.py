26c59ec1c0601b89b2baacbdd8731430e5bfb30c
Lupuna <terragod427@gmail.com>
2024-05-19 17:41:46 +0700

    first commit

--- two_personal_area/two_personal_area/settings.py
+++ two_personal_area/two_personal_area/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'customer.apps.CustomerConfig',
-    'employee.apps.EmployeeConfig',
+    'account.apps.AccountConfig',
 ]
 
 MIDDLEWARE = [
@@ -113,6 +112,8 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+AUTH_USER_MODEL = 'account.Client'
 
 
 # Static files (CSS, JavaScript, Images)
